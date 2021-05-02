8e3a673cba504bb587ff1a50f5bbc5b540d090d8
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2021-05-03 02:18:25 +0800

    Put enttrypoint to /sbin

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM centos:7
 LABEL maintainer="Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>"
-WORKDIR /app
 
 ARG USER_ID=14
 ARG GROUP_ID=50
@@ -15,15 +14,15 @@ RUN yum install -y \
 RUN usermod -u ${USER_ID} ftp
 RUN groupmod -g ${GROUP_ID} ftp
 
-ENV FTP_USER bayudwiyansatria
-ENV FTP_PASS bayudwiyansatria
-ENV PASV_ADDRESS localhost
+ENV FTP_USER **String**
+ENV FTP_PASS **Random**
+ENV PASV_ADDRESS **IPv4**
 ENV PASV_ADDR_RESOLVE NO
 ENV PASV_ENABLE YES
 ENV PASV_MIN_PORT 21100
 ENV PASV_MAX_PORT 21110
 ENV XFERLOG_STD_FORMAT NO
-ENV LOG_STDOUT bayudwiyansatria
+ENV LOG_STDOUT **Boolean**
 ENV FILE_OPEN_MODE 0666
 ENV LOCAL_UMASK 077
 ENV REVERSE_LOOKUP_ENABLE YES
@@ -32,8 +31,9 @@ ENV PORT_PROMISCUOUS NO
 
 COPY vsftpd.conf /etc/vsftpd/
 COPY vsftpd_virtual /etc/pam.d/
-COPY entrypoint.sh .
+COPY run-vsftpd.sh /usr/sbin/
 
+RUN chmod +x /usr/sbin/run-vsftpd.sh
 RUN mkdir -p /home/vsftpd/
 RUN chown -R ftp:ftp /home/vsftpd/
 
@@ -42,4 +42,4 @@ VOLUME /var/log/vsftpd
 
 EXPOSE 20 21
 
-ENTRYPOINT [ "./entrypoint.sh" ]
+CMD ["/usr/sbin/run-vsftpd.sh"]
--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-if [ "$FTP_USER" = "bayudwiyansatria" ]; then
-	export FTP_USER='admin'
+# If no env var for FTP_USER has been specified, use 'admin':
+if [ "$FTP_USER" = "**String**" ]; then
+    export FTP_USER='admin'
 fi
 
-if [ "$FTP_PASS" = "bayudwiyansatria" ]; then
-	export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
+# If no env var has been specified, generate a random password for FTP_USER:
+if [ "$FTP_PASS" = "**Random**" ]; then
+    export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
 fi
 
-if [ "$LOG_STDOUT" = "bayudwiyansatria" ]; then
-	export LOG_STDOUT=''
+# Do not log to STDOUT by default:
+if [ "$LOG_STDOUT" = "**Boolean**" ]; then
+    export LOG_STDOUT=''
 else
-	export LOG_STDOUT='Yes.'
+    export LOG_STDOUT='Yes.'
 fi
 
+# Create home dir and update vsftpd user db:
 mkdir -p "/home/vsftpd/${FTP_USER}"
 chown -R ftp:ftp /home/vsftpd/
 
 echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
-if [ "$PASV_ADDRESS" = "localhost" ]; then
-	export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
+# Set passive mode parameters:
+if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
+    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
 fi
 
 echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
@@ -36,8 +41,10 @@ echo "reverse_lookup_enable=${REVERSE_LOOKUP_ENABLE}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
 echo "port_promiscuous=${PORT_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
 
+# Get log file path
 export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
 
+# stdout server info:
 if [ ! $LOG_STDOUT ]; then
 cat << EOB
 	SERVER SETTINGS
@@ -48,7 +55,8 @@ cat << EOB
 	· Redirect vsftpd log to STDOUT: No.
 EOB
 else
-	/usr/bin/ln -sf /dev/stdout $LOG_FILE
+    /usr/bin/ln -sf /dev/stdout $LOG_FILE
 fi
 
+# Run vsftpd:
 &>/dev/null /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
